b6a620f86bc4766dabadaa7da5c159c9894bb4c6
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-06-19 17:03:10 +0900

    chore: add ESLint and Prettier setup

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,26 +1,26 @@
 import { defineConfig } from 'vite';
-import inertia from '@inertiajs/vite'
+import inertia from '@inertiajs/vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.ts'],
+      refresh: true,
+      fonts: [
+        bunny('Instrument Sans', {
+          weights: [400, 500, 600],
         }),
-        inertia(),
-        tailwindcss(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
+      ],
+    }),
+    inertia(),
+    tailwindcss(),
+  ],
+  server: {
+    watch: {
+      ignored: ['**/storage/framework/views/**'],
     },
+  },
 });
