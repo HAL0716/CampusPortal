633757c995064956986183dc1e3514f2aaf64932
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-07-20 08:29:47 +0900

    feat: and RoleSeeder with RoleType enum

--- src/app/Models/Role.php
+++ src/app/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Domain\Role\RoleType;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,6 +10,18 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 #[Fillable(['name'])]
 class Role extends Model
 {
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'name' => RoleType::class,
+        ];
+    }
+
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'user_role')
