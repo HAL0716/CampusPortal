1697141c4608337a916a3e9fa4057208a5a0a371
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-07-20 09:07:05 +0900

    feat: add AdminSeeder and update configuration for admin credentials

--- src/config/seeding.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'test_user' => [
-        'name' => env('SEED_TEST_USER_NAME'),
-        'email' => env('SEED_TEST_USER_EMAIL'),
-        'password' => env('SEED_TEST_USER_PASSWORD'),
-    ],
-];
