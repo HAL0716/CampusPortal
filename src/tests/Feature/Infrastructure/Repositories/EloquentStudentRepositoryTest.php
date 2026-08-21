77b23981699b6f49856066634f7ffec935f1685c
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-08-21 16:49:47 +0900

    refactor: add getByUserId method to StudentRepository

--- src/tests/Feature/Infrastructure/Repositories/EloquentStudentRepositoryTest.php
+++ src/tests/Feature/Infrastructure/Repositories/EloquentStudentRepositoryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Infrastructure\Repositories;
 
 use App\Domain\Student\Entities\Student;
+use App\Domain\Student\Exceptions\StudentNotFoundException;
 use App\Domain\User\ValueObjects\UserId;
 use App\Infrastructure\Repositories\EloquentStudentRepository;
 use App\Models\Student as StudentModel;
@@ -38,5 +39,23 @@ final class EloquentStudentRepositoryTest extends TestCase
         $found = $this->repository->findByUserId(new UserId(999));
 
         self::assertNull($found);
+    }
+
+    public function test_can_get_student_by_user_id(): void
+    {
+        $student = StudentModel::factory()->create();
+
+        $found = $this->repository->getByUserId(new UserId($student->user_id));
+
+        self::assertInstanceOf(Student::class, $found);
+        self::assertSame($student->id, $found->id()->value());
+        self::assertSame($student->user_id, $found->userId()->value());
+    }
+
+    public function test_throws_exception_when_student_not_found_by_user_id(): void
+    {
+        $this->expectException(StudentNotFoundException::class);
+
+        $this->repository->getByUserId(new UserId(999999));
     }
 }
