8a8195b0e47c070233546760c766ef2609bda4bb
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-09-15 15:59:47 +0900

    feat: implement getDetail method in EloquentSemesterQueryService and add corresponding unit test

--- src/app/Infrastructure/QueryServices/EloquentSemesterQueryService.php
+++ src/app/Infrastructure/QueryServices/EloquentSemesterQueryService.php
@@ -2,13 +2,61 @@
 
 namespace App\Infrastructure\QueryServices;
 
+use App\Application\Contexts\Semester\DTOs\CourseOfferingDTO;
+use App\Application\Contexts\Semester\DTOs\SemesterDetailDTO;
 use App\Application\Contexts\Semester\DTOs\SemesterDTO;
 use App\Application\Contexts\Semester\Services\SemesterQueryService;
 use App\Domain\Semester\Exceptions\SemesterNotFoundException;
+use App\Domain\Semester\ValueObjects\SemesterId;
 use App\Models\Semester;
 
 final readonly class EloquentSemesterQueryService implements SemesterQueryService
 {
+    public function getDetail(SemesterId $semesterId): SemesterDetailDTO
+    {
+        $semester = Semester::query()
+            ->select([
+                'id',
+                'academic_year',
+                'term',
+                'start_date',
+                'end_date',
+            ])
+            ->with([
+                'courseOfferings' => fn ($query) => $query
+                    ->select([
+                        'id',
+                        'course_id',
+                        'semester_id',
+                    ])
+                    ->with([
+                        'course:id,name,description',
+                    ]),
+            ])
+            ->find($semesterId->value());
+
+        if ($semester === null) {
+            throw new SemesterNotFoundException;
+        }
+
+        return new SemesterDetailDTO(
+            id: $semester->id,
+            academicYear: $semester->academic_year,
+            term: $semester->term->value,
+            startDate: $semester->start_date->format('Y-m-d'),
+            endDate: $semester->end_date->format('Y-m-d'),
+            courseOfferings: $semester->courseOfferings
+                ->map(
+                    fn ($courseOffering) => new CourseOfferingDTO(
+                        id: $courseOffering->id,
+                        name: $courseOffering->course->name,
+                        description: $courseOffering->course->description,
+                    )
+                )
+                ->all(),
+        );
+    }
+
     /** @return array<SemesterDTO> */
     public function findAll(): array
     {
--- src/tests/Feature/Infrastructure/QueryServices/EloquentSemesterQueryServiceTest.php
+++ src/tests/Feature/Infrastructure/QueryServices/EloquentSemesterQueryServiceTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Feature\Infrastructure\QueryServices;
 
+use App\Application\Contexts\Semester\DTOs\CourseOfferingDTO;
+use App\Application\Contexts\Semester\DTOs\SemesterDetailDTO;
 use App\Application\Contexts\Semester\DTOs\SemesterDTO;
 use App\Domain\Academic\Enums\Term;
+use App\Domain\Semester\ValueObjects\SemesterId;
 use App\Infrastructure\QueryServices\EloquentSemesterQueryService;
+use App\Models\CourseOffering;
 use App\Models\Semester;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -20,6 +24,37 @@ final class EloquentSemesterQueryServiceTest extends TestCase
         parent::setUp();
 
         $this->queryService = new EloquentSemesterQueryService;
+    }
+
+    public function test_get_detail_returns_semester_detail(): void
+    {
+        $semester = Semester::factory()->create([
+            'academic_year' => '2026',
+            'term' => Term::FIRST,
+            'start_date' => '2026-04-01',
+            'end_date' => '2026-08-31',
+        ]);
+
+        $courseOfferings = CourseOffering::factory()->for($semester)->create();
+
+        $expected = new SemesterDetailDTO(
+            id: $semester->id,
+            academicYear: $semester->academic_year,
+            term: $semester->term->value,
+            startDate: $semester->start_date->format('Y-m-d'),
+            endDate: $semester->end_date->format('Y-m-d'),
+            courseOfferings: [
+                new CourseOfferingDTO(
+                    id: $courseOfferings->id,
+                    name: $courseOfferings->course->name,
+                    description: $courseOfferings->course->description,
+                ),
+            ],
+        );
+
+        $result = $this->queryService->getDetail(new SemesterId($semester->id));
+
+        self::assertEquals($expected, $result);
     }
 
     public function test_find_all_returns_semesters_in_order(): void
